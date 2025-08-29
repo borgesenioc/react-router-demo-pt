cf6df0c09da799b27a88200e842dfe3d68b4d3ad
borgesenioc <borgesenioc@gmail.com>
2025-08-29 09:04:26 -0300

    display the dashboard as expected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './Header/Header';
+import Dashboard from './Dashboard/Dashboard';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
